44e2dc0aca0467143e504585e7cfdddef37e245a
= <=>
2021-03-11 20:18:48 +0200

    .h description

--- binary_trees.h
+++ binary_trees.h
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 
 struct binary_tree_s
 {
