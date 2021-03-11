60861de906ea08f4fd96720f0de05396b657ae37
= <=>
2021-03-11 15:15:50 +0200

    checkout binary tree and remove the loop. only create a new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,43 +9,12 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *tmp, *current, *p;
+binary_tree_t *tmp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+if (!tmp)
+return (NULL);
 
-tmp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 tmp->n = value;
+tmp->parent = parent;
 
-if (!parent)
-{
-parent = tmp;
-return (parent);
-}
-
-current = parent, p = NULL;
-
-while (1)
-{
-p = current;
-
-if (value < p->n)
-{
-current = current->left;
-
-if (!current)
-{
-p->left = tmp, tmp->parent = p;
 return (tmp);
-}
-}
-
-else
-{
-current = current->right;
-
-if (!current)
-{
-p->right = tmp, tmp->parent = p;
-return (tmp);
-}
-}
-}
 }
